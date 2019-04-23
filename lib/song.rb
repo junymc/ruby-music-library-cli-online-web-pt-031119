2a88a3ba158922cd99cd1c3d3c1b990ca1130f8a
Junyoung McArdle <juny.mcardle@gmail.com>
2019-04-23 05:17:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,10 +10,10 @@ class Song
     self.genre = genre if genre
   end
 
-  # def artist=(artist)
-  #   @artist = artist
-  #   artist.add_song(self)
-  # end
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
 
   def self.all
     @@all
