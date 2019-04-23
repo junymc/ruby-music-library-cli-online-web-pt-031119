6bcb349f8523811d812afd6e534a61905006009a
Junyoung McArdle <juny.mcardle@gmail.com>
2019-04-23 05:10:18 +0000

    Automatically backed up by Learn

--- lib/concerns/musiclibrarycontroller.rb
+++ lib/concerns/musiclibrarycontroller.rb
@@ -28,8 +28,8 @@ class MusicLibraryController
         list_genres
       when "list artist"
         list_songs_by_artist
-      # when "list genre"
-      #   list_songs_by_genre
+      when "list genre"
+        list_songs_by_genre
       # when "play song"
       #   play_song
       end
